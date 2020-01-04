3dae086add889bae56ba9a1aa28654d8d983c523
yurakhomitsky <yura.khomitsky8@gmail.com>
2020-01-04 12:25:06 +0200

    Create CRUD for Faculty:

--- src/app/admin/admin-panel-page/admin-panel.component.ts
+++ src/app/admin/admin-panel-page/admin-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-panel',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  ToFaculty() {
+    this.router.navigate(['admin/faculty']);
+}
 }
