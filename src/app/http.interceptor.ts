cf74d805598e68aeda27d7286f165974aeb18b84
v.chykharivskyi <v.chykharivskyi@gmail.com>
2020-01-05 01:09:32 +0200

    Removed console log

--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -5,8 +5,8 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ApiHttpInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('cdsdfdf');
     const apiReq = request.clone({ url: `http://dtapi.if.ua/${request.url}` });
+
     return next.handle(apiReq);
   }
 }
