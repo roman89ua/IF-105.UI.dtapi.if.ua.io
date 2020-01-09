45ac951648bd592bd559fcf0b3850277ca1ebe8a
Romeo Petryshyn <romeopetryshyn@gmail.com>
2020-01-09 22:07:22 +0200

    [REBUILD 2.1] Added needed files, left lazy loading for admin which can be used as an example for lazy loading of students module, set up roating for entities inside admin without lazy loading

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminPanelComponent } from './admin-panel-page/admin-panel.component';
+import { AdminComponent } from './admin.component';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
 
 
-const adminRoutes: Routes = [
-  { path: '', component: AdminPanelComponent },
+const routes: Routes = [
+  { path: '', component: AdminComponent , 
+    children:[
+      { path: 'students', component: StudentsComponent }
+      // Here you can add routes for your entities
+  ]}
 ];
 @NgModule({
-  declarations: [AdminPanelComponent],
+  declarations: [
+    AdminComponent,
+    StudentsComponent
+  ],
   imports: [
     CommonModule,
-    RouterModule.forChild(adminRoutes),
+    RouterModule.forChild(routes),
   ]
 })
 export class AdminModule { }
