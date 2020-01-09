0a9733517fb302cf189d38c7d391e48467063c06
Romeo Petryshyn <romeopetryshyn@gmail.com>
2020-01-09 15:33:48 +0200

    [REBUILD 2.0] Set up routing in admin without lazy loading. Removed unneeded components for now. Lazy loading for admin itself after login page was left

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminComponent } from './admin.component';
+import { AdminPanelComponent } from './admin-panel-page/admin-panel.component';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const adminRoutes: Routes = [
-  {
-    path: '',
-        component: AdminComponent,
-        children: [
-          { path: 'students', loadChildren: () => import('./students/students.module').then(m => m.StudentsModule) },
-          { path: 'subjects', loadChildren: () => import('./subjects/subjects.module').then(m => m.SubjectsModule) },
-          { path: 'groups', loadChildren: () => import('./groups/groups.module').then(m => m.GroupsModule) },
-          { path: 'faculties', loadChildren: () => import('./faculties/faculties.module').then(m => m.FacultiesModule) },
-          { path: 'specialities', loadChildren: () => import('./specialities/specialities.module').then(m => m.SpecialitiesModule) },
-        ]
-  },
+  { path: '', component: AdminPanelComponent },
 ];
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminPanelComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(adminRoutes),
