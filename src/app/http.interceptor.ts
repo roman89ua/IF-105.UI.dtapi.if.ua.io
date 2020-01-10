4d2134a939863be7b3f4f7325fa3a7e8f98cd607
v.chykharivskyi <v.chykharivskyi@gmail.com>
2020-01-10 22:43:53 +0200

    Changed backend api url

--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ApiHttpInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const apiReq = request.clone({ url: `http://dtapi.if.ua/${request.url}` });
+    const apiReq = request.clone({ url: `http://dtapi.if.ua/api/${request.url}` });
 
     return next.handle(apiReq);
   }
