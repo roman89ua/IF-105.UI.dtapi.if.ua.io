da433368e0d2a73d3ee8b4915b463e2255a90d9c
Tania Kravchuk <tatka.if@gmail.com>
2020-01-19 17:10:20 +0200

    Added logo for the login form

--- src/app/login/login-page/login.component.ts
+++ src/app/login/login-page/login.component.ts
@@ -4,6 +4,8 @@ import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { Router } from '@angular/router';
 import { AuthService } from '../../shared/auth.service';
+import { LoginService } from '../login.service';
+import { SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
@@ -14,10 +16,21 @@ export class LoginComponent implements OnInit {
 
   public userData: { username: string; password: string; } = { username: null, password: null };
   public error = null;
+  public logo: SafeResourceUrl;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private loginService: LoginService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.getLogo();
+  }
+
+  getLogo() {
+    this.loginService.getLogo()
+      .subscribe(data => this.logo = data);
   }
   login() {
     this.authService
