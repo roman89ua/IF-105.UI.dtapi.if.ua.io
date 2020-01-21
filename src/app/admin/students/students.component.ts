bab2a9bd96546d48145da0ff44e3de1c97821dac
KostyaKravtsiv <kostya.kravtsiv@gmail.com>
2020-01-21 23:27:27 +0200

    [EDIT] refactored students to use single crud service

--- src/app/admin/students/students.component.ts
+++ src/app/admin/students/students.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-// import { DialogService } from 'src/app/shared/dialog.service';
-=======
->>>>>>> 523956a01266056a7f20440d2316030816d4abe4
 import { ActivatedRoute } from '@angular/router';
 import { MatDialog, MatSnackBar } from '@angular/material';
-
 import { ModalService } from '../../shared/services/modal.service';
-
 import { StudentsModalWindowComponent } from './students-modal-window/students-modal-window.component';
-import { StudentsService } from './services/students.service';
 import { GetStudentsInterface } from './interfaces/get-students-interface';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-students',
@@ -24,11 +18,7 @@ export class StudentsComponent implements OnInit {
   public displayedColumns: string[] = ['gradebookID', 'studentName', 'studentSurname', 'UpdateDelete'];
 
   constructor(
-    private studentsHttpService: StudentsService,
-<<<<<<< HEAD
-    // public dialogService: DialogService,
-=======
->>>>>>> 523956a01266056a7f20440d2316030816d4abe4
+    private apiService: ApiService,
     public dialog: MatDialog,
     private activatedRoute: ActivatedRoute,
     private modalService: ModalService,
@@ -42,30 +32,24 @@ export class StudentsComponent implements OnInit {
   // Students showing
   showStudentsByGroup() {
     this.groupdID = this.activatedRoute.snapshot.params['id'];
-    this.studentsHttpService.getStudentsByGroup(this.groupdID).subscribe((result: Array<GetStudentsInterface>) => {
+    this.apiService.getEntityByAction('Student', 'getStudentsByGroup', this.groupdID).subscribe((result: Array<GetStudentsInterface>) => {
       this.STUDENTS_LIST = result;
     });
   }
-<<<<<<< HEAD
-  openConfirmDialog(name: string, surname: string, id: string) {
-    const message = `Підтвердіть видалення користувача "${surname} ${name}"`;
-    this.modalService.openConfirmModal(message, () => this.delStudent(id));
-=======
   ////////////////////////
 
   // Students Adding
   addStudent(): void {
     this.showModalWindow().afterClosed().subscribe((response: any) => {
       if (response) {
-          if (response.response === 'ok') {
-            this.showSnackBar('Студент доданий, дані збережено');
-            this.showStudentsByGroup();            
-          } else if (response.error || response.response === 'Failed to validate array') {
-            this.showSnackBar('ПОМИЛКА');
-          }
+        if (response.response === 'ok') {
+          this.showSnackBar('Студент доданий, дані збережено');
+          this.showStudentsByGroup();
+        } else if (response.error || response.response === 'Failed to validate array') {
+          this.showSnackBar('ПОМИЛКА');
+        }
       }
     });
->>>>>>> 523956a01266056a7f20440d2316030816d4abe4
   }
   ////////////////////////
 
@@ -73,7 +57,7 @@ export class StudentsComponent implements OnInit {
   // Students removing
   deleteStudent(id: string) {
     const id_num = Number.parseInt(id);
-    this.studentsHttpService.deleteStudent(id_num).subscribe((data: { response?: string; } ) => {
+    this.apiService.delEntity('Student', id_num).subscribe((data: { response?: string; }) => {
       if (data && data.response === 'ok') {
         this.STUDENTS_LIST = this.STUDENTS_LIST.filter(student => student.user_id !== id);
         this.showSnackBar('Студент видалений, дані збережено');
@@ -83,7 +67,7 @@ export class StudentsComponent implements OnInit {
 
   openConfirmDialog(name: string, surname: string, id: string) {
     const message = `Підтвердіть видалення користувача "${surname} ${name}"`;
-    this.modalService.openConfirmModal(message, () => this.deleteStudent(id));  
+    this.modalService.openConfirmModal(message, () => this.deleteStudent(id));
   }
   ////////////////////////
 
@@ -101,7 +85,7 @@ export class StudentsComponent implements OnInit {
       width: '600px',
       height: 'calc(100vh - 50px)',
       data: {
-          group_id: this.groupdID,
+        group_id: this.groupdID,
       }
     });
   }
