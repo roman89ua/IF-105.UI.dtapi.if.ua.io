22e534e47b30da41a220185b9cb07144332d6075
Romeo Petryshyn <romeopetryshyn@gmail.com>
2020-01-23 19:43:14 +0200

    [UPDATE STUDENT CRUD] Refactored code to use api service in shared module, refactored code for easier reading, run tsLint and ngLing, tested everything once again and fixed some minor bugs

--- src/app/admin/students/services/students.service.ts
+++ src/app/admin/students/services/students.service.ts
@@ -1,18 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { GetStudentsInterface } from '../interfaces/get-students-interface';
-import { CreareUpdateStudentsInterface } from '../interfaces/creare-update-students-interface';
-import { ResponseInterface } from '../interfaces/response-interface';
 
 @Injectable()
 export class StudentsService {
-
-  private createStudentUrl = 'Student/insertData';
-  private readStudentUrl = 'Student/getStudentsByGroup';
-  private updateStudentUrl = 'Student/update';
-  private deleteStudentUrl = 'Student/del';
-  private getUserInfoURL = 'AdminUser/getRecords';
 
   private checkStudentNameURL = 'AdminUser/checkUserName';
   private checkStudentEmailURL = 'AdminUser/checkEmailAddress';
@@ -20,26 +10,6 @@ export class StudentsService {
   private checkGradebookIdURL = 'Student/checkGradebookID';
 
   constructor(private httpClient: HttpClient) {}
-
-  getStudentsByGroup(id: number): Observable<GetStudentsInterface[] & ResponseInterface> {
-    return this.httpClient.get<GetStudentsInterface[] & ResponseInterface>(`${this.readStudentUrl}/${id}`);
-  }
-
-  deleteStudent(id: number): Observable<ResponseInterface> {
-    return this.httpClient.get<ResponseInterface>(`${this.deleteStudentUrl}/${id}`);
-  }
-
-  createStudent(body): Observable<CreareUpdateStudentsInterface|ResponseInterface> {
-    return this.httpClient.post<CreareUpdateStudentsInterface|ResponseInterface>(`${this.createStudentUrl}`, body);
-  }
-
-  updateStudent(id: number, body): Observable<CreareUpdateStudentsInterface|ResponseInterface> {
-    return this.httpClient.post<CreareUpdateStudentsInterface|ResponseInterface>(`${this.updateStudentUrl}/${id}`, body);
-  }
-
-  getUserInfo(id: string){
-    return this.httpClient.get(`${this.getUserInfoURL}/${id}`);
-  }
 
   checkUsername(username: string) {
     return this.httpClient.get(`${this.checkStudentNameURL}/${username}`);
@@ -49,7 +19,7 @@ export class StudentsService {
     return this.httpClient.get(`${this.checkStudentEmailURL}/${email}`);
   }
 
-  checkGradebookID(gradebook_id: string) {
-    return this.httpClient.get(`${this.checkGradebookIdURL}/${gradebook_id}`);
+  checkGradebookID(gradebookID: string) {
+    return this.httpClient.get(`${this.checkGradebookIdURL}/${gradebookID}`);
   }
 }
