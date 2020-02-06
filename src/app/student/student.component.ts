4eefdfba07aaad6e75a3f8031cb9e2bf912ee5d7
Tania Kravchuk <tatka.if@gmail.com>
2020-02-07 00:28:39 +0200

    test-player

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-student',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    const currentUser = this.authService.getCurrentUser();
   }
 
 }
