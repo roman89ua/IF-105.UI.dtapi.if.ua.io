15e2167e554472c5e3c84a75563351d246235b3f
Serhii Zikratyi <zikratiy@gmail.com>
2020-02-24 09:13:28 +0200

    [ADD] added componet result-question detail

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -48,6 +48,7 @@ import { ScoreAnswerPipe } from './results/pipes/score-answer.pipe';
 import { ResultGroupRaitingComponent } from './results/result-group-raiting/result-group-raiting.component';
 import { TestDetailListComponent } from './test-detail/list/test-detail-list.component';
 import { TestDetailAddComponent } from './test-detail/add/test-detail-add.component';
+import { ResultsQuestionDetailComponent } from './results/results-question-detail/results-question-detail.component';
 
 
 const routes: Routes = [
@@ -184,6 +185,7 @@ const routes: Routes = [
     ResultDetailComponent,
     ScoreAnswerPipe,
     ResultGroupRaitingComponent,
+    ResultsQuestionDetailComponent,
 
   ],
   imports: [
