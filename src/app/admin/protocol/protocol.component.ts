86384c62ca6ece11d64627e027c4c2943e4c580d
KostyaKravtsiv <kostya.kravtsiv@gmail.com>
2020-03-04 21:21:38 +0200

    [ADD] logic for protocol page

--- src/app/admin/protocol/protocol.component.ts
+++ src/app/admin/protocol/protocol.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ProtocolService } from './protocol.service';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { mergeMap } from 'rxjs/operators';
+import { forkJoin, of } from 'rxjs';
+import { Protocol } from './protocol.interface';
+import { MatTableDataSource, MatTable, MatPaginator, MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-protocol',
   templateUrl: './protocol.component.html',
   styleUrls: ['./protocol.component.scss']
 })
-export class ProtocolComponent implements OnInit {
+export class ProtocolComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  public displayedColumns: string[] = ['userId', 'userName', 'testName', 'ipAddress', 'date', 'time'];
+  public dataSource = new MatTableDataSource<Protocol>();
+
+  @ViewChild('table', { static: false }) table: MatTable<Element>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  logsDateForm: FormGroup;
+  constructor(private protocolService: ProtocolService, private fb: FormBuilder, private snackBar: MatSnackBar, ) { }
 
   ngOnInit() {
+    this.logsDateForm = this.fb.group({
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required, [this.checkForCorrectData()]]
+    });
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onSubmit() {
+    this.protocolService.getLogs(this.logsDateForm.value).pipe(
+      mergeMap(res1 => {
+        const tests = this.protocolService.getTests(this.protocolService.getTestIds(res1));
+        const users = this.protocolService.getUsers(this.protocolService.getUserIds(res1));
+        return forkJoin([of(res1), tests, users]);
+      })
+    ).subscribe(result => {
+      result[0].length < 1 ? this.openSnackBar('Даних не знайдено') :
+        this.dataSource.data = this.protocolService.getProtocolObj(result);
+    });
+  }
+
+  openSnackBar(message: string, action?: string) {
+    this.snackBar.open(message, action, {
+      duration: 2500,
+    });
+  }
+/** checks if endDate is bigger than startDate */
+  checkForCorrectData() {
+    return (control: FormControl) => {
+      if (new Date(control.value) < new Date(this.logsDateForm.value.startDate)) {
+        return of({ dataIsNotCorrect: true });
+      } else { return of(null); }
+    };
+  }
+
+  hasError = (controlName: string, errorName: string) => {
+    return this.logsDateForm.controls[controlName].hasError(errorName);
+  }
 }
---
+++ src/app/admin/protocol/protocol.interface.ts
@@ -0,0 +1,16 @@
+export interface Protocol {
+  userId;
+  userName;
+  ipAddress;
+  date;
+  time;
+  testName;
+}
+export interface Log {
+  log_id;
+  user_id;
+  test_id;
+  log_date;
+  log_time;
+  remote_ip;
+}
