338d147266f810f6b0b428e6ea45d5e89d83d85b
Romeo Petryshyn <romeopetryshyn@gmail.com>
2020-03-04 19:07:00 +0200

    [CREATED ABOUT US COMPONENT] Added some information about project and created 2 charts based on project information which received from github API. Will continue with Our team section to finish component.

--- src/app/admin/about-us/about-us.component.ts
+++ src/app/admin/about-us/about-us.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Bezgachnyuk,
+  Petryshyn,
+  Shevchyk,
+  Kostiuk,
+  Khomitskyi,
+  Kravchuk,
+  Zikratyi,
+  Kravtsiv,
+  Chykharivskyi
+} from './photos';
 
 @Component({
   selector: 'app-about-us',
@@ -6,6 +17,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+
+  public bezgachnyuk = Bezgachnyuk;
+  public petryshyn = Petryshyn;
+  public shevchyk = Shevchyk;
+  public kostiuk = Kostiuk;
+  public khomitskyi = Khomitskyi;
+  public kravchuk = Kravchuk;
+  public zikratyi = Zikratyi;
+  public kravtsiv = Kravtsiv;
+  public chykharivskyi = Chykharivskyi;
 
   constructor() { }
 
